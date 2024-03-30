61d584e272e76b321e84f7ee268fac476ccc3c84
noelwesleey64 <noelwesley8750@gmail.com>
2024-03-30 11:39:59 +0300

    server

--- Api/Customers/customers.router.js
+++ Api/Customers/customers.router.js
@@ -2,10 +2,11 @@ const router = require("express").Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const sql = require("mssql/msnodesqlv8");
+const nodemailer = require("nodemailer");
 
 //Create Account
 router.post("/signup", async (req, res) => {
-  const customerID = Math.random(Math.floor() * 10000);
+  const customerID = Math.floor(Math.random() * 100000);
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
   const email = req.body.email;
@@ -42,9 +43,88 @@ router.post("/signup", async (req, res) => {
       }
     }
   );
-
 });
 
-router
+//Login
+router.post("/signin", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  new sql.Request().query(
+    `select * from Customers where Email = '${email}'`,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Internal Server Error");
+      } else if (result.recordsets[0].length === 0) {
+        console.log(result);
+        res.status(401).send("Incorrect Email or Password");
+      } else if (result.recordsets[0].length > 0) {
+        const compare = bcrypt.compareSync(
+          password,
+          result.recordset[0].Password
+        );
+        if (compare) {
+          res.status(200).send("Login Successful");
+        } else {
+          res.status(401).send("Incorrect Email or Password");
+        }
+      }
+
+      console.log(result);
+    }
+  );
+});
+
+//Update Password
+router.put("/resetpassword", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  new sql.Request().query(
+    `update Customers set Password = '${hashedPassword}' where Email = '${email}'`,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      if (result.recordsets) {
+        console.log(result);
+        res.status(200).send("Password updated successfully");
+      }
+    }
+  );
+});
+
+//Email Verification
+router.post("/verifyemail", async (req, res) => {
+  const email = req.body.email;
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // Use `true` for port 465, `false` for all other ports
+    auth: {
+      user: "tillman.prohaska@ethereal.email",
+      pass: "PcPNhTsKnDnsRguKRn",
+    },
+  });
+
+  // async..await is not allowed in global scope, must use a wrapper
+  async function main() {
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+      from: '"Utibu Health" <tillman.prohaska@ethereal.email">', // sender address
+      to: email, // list of receivers
+      subject: "Hello ✔", // Subject line
+      text: "Hello world?", // plain text body
+      html: "<b>Hello world?</b>", // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
+  }
+  main();
+  // res.status(200).message("sent")
+});
 
 module.exports = router;
